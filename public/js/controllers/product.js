92d8d4de09174a8429bc074ee649debfe207ded1
Abel Separovich <separovi@gmail.com>
2017-09-23 17:48:13 -0300

    Ultima versão, não funciona, mas é isso ai =/

--- public/js/controllers/product.js
+++ public/js/controllers/product.js
@@ -10,6 +10,8 @@ angular.module('main').controller('productController', function($scope, $http){
         });
     };
 
+
+
     $scope.submeter = function() {
         console.log($scope.product);
     };
--- public/js/controllers/products-ctrl.js
+++ public/js/controllers/products-ctrl.js
@@ -1,5 +1,9 @@
-angular.module('main').controller('productCtrl', function($scope, $http){
+angular.module('main')
+.controller('productCtrl', function($scope, $http){
+
     $scope.produtos = [];
+    $scope.mensagem = '';
+    $scope.editando = {};
 
     $http.get('/produtos')
        .then(function(retorno) {
@@ -8,4 +12,43 @@ angular.module('main').controller('productCtrl', function($scope, $http){
        .catch(function(erro) {
            console.log(erro);
        });
+
+//Isso aqui ta funcionando , só não sei pq o mongo não responde positivamente
+       $scope.remover = function(produto){
+           $http.delete('/produtos/' + produto._id)
+           .then(function(produto){
+               var indiceDoItem = $scope.produto.indexOf(produto)
+               $scope.produto.splice(indiceDoItem, 1);
+               console.log('produto ' + produto.titulo + " foi removido.");
+               $scope.mensagem = 'produto ' + produto.titulo + " foi removido com sucesso."
+           }).catch(function(erro){
+               console.log(erro);
+               console.log('Deu erro ao deletar o item ' + produto.titulo);
+                $scope.mensagem = 'produto ' + produto.titulo + " não foi removido =/"
+           });
+       };
+
+
+
+
+
+       $scope.editar = function(produto){
+           $scope.editando = produto;
+           console.log($scope.editando);
+           /*
+           $http.put('/produtos/' + produto._id)
+           .then(function(produto){
+               console.log('produto ' + produto.titulo + " foi removido.");
+               $scope.mensagem = 'produto ' + produto.titulo + " foi removido com sucesso."
+           }).catch(function(erro){
+               console.log(erro);
+               console.log('Deu erro ao deletar o item ' + produto.titulo);
+                $scope.mensagem = 'produto ' + produto.titulo + " não foi removido =/"
+           });*/
+       };
+
+
+
+
+
 });
